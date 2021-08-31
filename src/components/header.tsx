53c5218eb24b692ff979e5d3427239da130d4dc1
Ryoh827 <rn.gjt25@gmail.com>
2021-08-31 20:02:04 +0900

    Layoutファイル等の作成

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
+import { AppBar, Typography, Toolbar } from '@material-ui/core';
 
 const Header = (): JSX.Element => {
-  return <></>;
+  return (
+    <div className="header">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className="uma-tag-title">
+            ウマタッグ
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { FC } from 'react';
+import '../styles/layout.scss';
+import Header from './header';
 
 const Layout: FC = ({ children }): JSX.Element => {
   return (
-    <>
+    <div className="layout">
+      <Header />
       <main>{children}</main>
-    </>
+    </div>
   );
 };
 
